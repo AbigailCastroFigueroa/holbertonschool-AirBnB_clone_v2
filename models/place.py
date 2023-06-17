0fbd41f095d2285bccae16d1fc76cf32322e62f0
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-06-17 16:24:30 -0400

    Commenting the place_amenities

--- models/place.py
+++ models/place.py
@@ -6,12 +6,12 @@ from sqlalchemy.orm import relationship
 
 metadata = Base.metadata
 
-place_amenity = Table('place_amenity', metadata,
-                      Column('place_id', String(60), ForeignKey(
-                          'places.id'), primary_key=True, nullable=False),
-                      Column('amenity_id', String(60), ForeignKey(
-                          'amenities.id'), primary_key=True, nullable=False)
-                      )
+# place_amenity = Table('place_amenity', metadata,
+#                       Column('place_id', String(60), ForeignKey(
+#                           'places.id'), primary_key=True, nullable=False),
+#                       Column('amenity_id', String(60), ForeignKey(
+#                           'amenities.id'), primary_key=True, nullable=False)
+#                       )
 
 
 class Place(BaseModel, Base):
@@ -28,5 +28,5 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenities = relationship(
-        'Amenity', secondary=place_amenity, viewonly=False)
+    # amenities = relationship(
+    #     'Amenity', secondary=place_amenity, viewonly=False)
